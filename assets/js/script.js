34f9cb81f94f9817848112bfc93505c49259263f
mflaviusm <you@example.com>
2021-08-06 15:13:24 +0100

    add subtraction code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,9 +25,10 @@ function runGame(gameType) {
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
-    }
-    else if (gameType === 'multiply') {
+    } else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === 'subtract') {
+        displaySubstractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -63,6 +64,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, 'addition'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -91,7 +94,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubstractQestion() {
+function displaySubstractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 
 }
 
